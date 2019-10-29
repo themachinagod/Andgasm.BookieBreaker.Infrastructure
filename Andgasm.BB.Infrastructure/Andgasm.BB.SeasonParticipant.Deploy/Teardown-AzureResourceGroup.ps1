349b0ca9269cc92f2b7c0e21a17f5b62be39588b
Danny Brown <machinagod@googlemail.com>
2019-10-29 15:28:54 +0000

    try v4 api

--- Andgasm.BB.Infrastructure/Andgasm.BB.SeasonParticipant.Deploy/Teardown-AzureResourceGroup.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.SeasonParticipant.Deploy/Teardown-AzureResourceGroup.ps1
@@ -1,3 +1,3 @@
 Write-Output '', 'Starting removal of Season Participant API App Service...'
-Remove-AzureResource -ResourceGroupName BookieBreaker -ResourceName SeasonParticipantSvcAPITEST -ResourceType Microsoft.Web/sites
+Remove-AzResource -ResourceGroupName BookieBreaker -ResourceName SeasonParticipantSvcAPITEST -ResourceType Microsoft.Web/sites
 Write-Output '', 'Successfully removed app service instance...'

8954cbd1eb5d222699174497c4a0c946807746a8
Danny Brown <machinagod@googlemail.com>
2019-10-29 13:47:35 +0000

    added some logging to removal job

--- Andgasm.BB.Infrastructure/Andgasm.BB.ServiceBus.Deploy/Teardown-AzureResourceGroup.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.ServiceBus.Deploy/Teardown-AzureResourceGroup.ps1
@@ -1,1 +1,3 @@
+Write-Output '', 'Starting removal of service bus instance...'
 Remove-AzureRmServiceBusNamespace -ResourceGroup testresource -NamespaceName BookieBreakerTEST
+Write-Output '', 'Successfully removed service bus instance...'

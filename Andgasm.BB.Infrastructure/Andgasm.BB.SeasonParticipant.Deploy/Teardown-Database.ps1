f4b21bc9b55a9b833cfbff45d1e9e0800876fffe
Danny Brown <machinagod@googlemail.com>
2019-10-31 09:59:31 +0000

    ver fix

--- Andgasm.BB.Infrastructure/Andgasm.BB.SeasonParticipant.Deploy/Teardown-Database.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.SeasonParticipant.Deploy/Teardown-Database.ps1
@@ -1,3 +1,3 @@
 Write-Output '', 'Starting removal of Season Participant Database...'
-Remove-AzureRmSqlDatabase -ResourceGroupName BookieBreaker -ServerName bookiebreakersql -DatabaseName SeasonParticipantSvc -Force
+Remove-AzSqlDatabase -ResourceGroupName BookieBreaker -ServerName bookiebreakersql -DatabaseName SeasonParticipantSvc -Force
 Write-Output '', 'Successfully removed database instance...'

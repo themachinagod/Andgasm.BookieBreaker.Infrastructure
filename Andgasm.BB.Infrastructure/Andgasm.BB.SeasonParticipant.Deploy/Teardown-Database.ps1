7f451f9db4a55bb0e5fa1d5261e8856640873072
Danny Brown <machinagod@googlemail.com>
2019-10-31 12:38:51 +0000

    catch no db

--- Andgasm.BB.Infrastructure/Andgasm.BB.SeasonParticipant.Deploy/Teardown-Database.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.SeasonParticipant.Deploy/Teardown-Database.ps1
@@ -1,3 +1,10 @@
 Write-Output '', 'Starting removal of Season Participant Database...'
-Remove-AzSqlDatabase -ResourceGroupName BookieBreaker -ServerName bookiebreakersql -DatabaseName SeasonParticipantSvc -Force
-Write-Output '', 'Successfully removed database instance...'
+try
+{
+	Remove-AzSqlDatabase -ResourceGroupName BookieBreaker -ServerName bookiebreakersql -DatabaseName SeasonParticipantSvc -Force
+	Write-Output '', 'Successfully removed database instance...'
+}
+catch
+{
+	Write-Output '', 'database instance does not exist...'
+}

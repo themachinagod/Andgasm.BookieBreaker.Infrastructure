312b1cd4999620d844d9e0785b0f9a237287d58b
Danny Brown <machinagod@googlemail.com>
2020-02-04 14:37:17 +0000

    case fix

--- Andgasm.BB.Infrastructure/Andgasm.BB.ServiceBus.Deploy/Teardown-AzureResourceGroup.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.ServiceBus.Deploy/Teardown-AzureResourceGroup.ps1
@@ -1,3 +1,3 @@
 Write-Output '', 'Starting removal of service bus instance...'
-Remove-AzureRmServiceBusNamespace -ResourceGroup BookieBreaker -NamespaceName BookieBreakereventBus
+Remove-AzureRmServiceBusNamespace -ResourceGroup BookieBreaker -NamespaceName BookieBreakerEventBus
 Write-Output '', 'Successfully removed service bus instance...'

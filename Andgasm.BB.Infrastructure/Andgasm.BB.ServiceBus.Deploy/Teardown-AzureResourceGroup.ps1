27bc34703a402bc9b07f079c50cf1568056002a0
Danny Brown <machinagod@googlemail.com>
2020-02-20 13:30:09 +0000

    fix for teardown of sb

--- Andgasm.BB.Infrastructure/Andgasm.BB.ServiceBus.Deploy/Teardown-AzureResourceGroup.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.ServiceBus.Deploy/Teardown-AzureResourceGroup.ps1
@@ -1,3 +1,10 @@
 Write-Output '', 'Starting removal of service bus instance...'
-Remove-AzureRmServiceBusNamespace -ResourceGroup BookieBreaker -NamespaceName BookieBreakerEventBus
-Write-Output '', 'Successfully removed service bus instance...'
+try
+{
+	Remove-AzureRmServiceBusNamespace -ResourceGroup BookieBreaker -NamespaceName BookieBreakerEventBus
+	Write-Output '', 'Successfully removed service bus instance...'
+}
+catch
+{
+	Write-Output '', 'Service bus instance does not exist...'
+}

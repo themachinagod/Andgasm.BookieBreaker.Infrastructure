321f266860966198c470ecd6912aea4ee81a6f77
Danny Brown <machinagod@googlemail.com>
2020-02-20 13:35:08 +0000

    fix for teardown sql instance

--- Andgasm.BB.Infrastructure/Andgasm.BB.SQLServer.Deploy/Teardown-AzureResourceGroup.ps1
+++ Andgasm.BB.Infrastructure/Andgasm.BB.SQLServer.Deploy/Teardown-AzureResourceGroup.ps1
@@ -1,3 +1,10 @@
 Write-Output '', 'Starting removal of SQL server instance...'
-Remove-AzureRmSqlServer -ResourceGroupName BookieBreaker -ServerName bookiebreakersql
-Write-Output '', 'Successfully removed SQL server instance...'
+try
+{
+	Remove-AzureRmSqlServer -ResourceGroupName BookieBreaker -ServerName bookiebreakersql
+	Write-Output '', 'Successfully removed SQL server instance...'
+}
+catch
+{
+	Write-Output '', 'SQL server instance does not exist...'
+}
